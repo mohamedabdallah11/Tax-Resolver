391f55920f71ee4520e40bfe9824267d5e5fc764
mohamed abdallah <mohamedabdallahh26@gmail.com>
2025-03-22 00:07:47 +0200

    Cashing with Redis for the returned taxes

--- src/Service/TaxService.php
+++ src/Service/TaxService.php
@@ -5,15 +5,23 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Factory\TaxProviderFactory;
+use Psr\Cache\CacheItemPoolInterface;
 class TaxService
 {
     public function __construct(
         public TaxProviderFactory $taxProvider,
+        private readonly CacheItemPoolInterface $cache
     ) {}
 
     public function getTaxes(string $country, ?string $state): array
     {
-      
+         
+        $cacheKey = "taxes_{$country}_{$state}";
+        $cacheItem = $this->cache->getItem($cacheKey);
+
+        if ($cacheItem->isHit()) {
+            return $cacheItem->get();
+        }
  
 
             $provider = $this->taxProvider->getProvider($country);
@@ -21,6 +29,9 @@ class TaxService
             $taxes = $provider->getTaxes($country, $state);
         
 
+         $cacheItem->set($taxes);
+        $cacheItem->expiresAfter(3600);
+        $this->cache->save($cacheItem);
 
         return $taxes;
     }
